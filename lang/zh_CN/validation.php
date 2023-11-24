d5279941fdce272658a8a4d25fa75f9b2b8582db
lebron <595452769@qq.com>
2023-11-24 03:08:21 +0000

    中文的错误提示信息

---
+++ lang/zh_CN/validation.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'attributes' => [
+            'name' => '名称',
+            'email' => '邮箱',
+            'password' => '密码'
+        ]
+    ];
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,7 @@
       <h5>注册</h5>
     </div>
     <div class="card-body">
+      @include('shared._errors')
       <form action="{{ route('users.store') }}" method="POST">
         {{ csrf_field() }}
         <div class="mb-3">
