1786253434a35043ddc835d39708de4b3599d5d2
lebron <595452769@qq.com>
2023-11-24 08:02:40 +0000

    更改用户资料

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,7 +10,7 @@
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a href="{{ route('users.show', Auth::user()) }}" class="dropdown-item">个人中心</a>
-            <a href="#" class="dropdown-item">编辑资料</a>
+            <a href="{{ route('users.edit', Auth::user()) }}" class="dropdown-item">编辑资料</a>
             <div class="dropdown-divider"></div>
             <a href="#" class="dropdown-item" id="logout">
               <form action="{{ route('logout') }}" method="post">
