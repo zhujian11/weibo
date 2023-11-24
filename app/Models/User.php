2d3bb50a131d36f3ef1189ccccd525f54c8d08a0
lebron <595452769@qq.com>
2023-11-24 02:03:44 +0000

    用户注册表单

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "https://cdn.v2ex.com/gravatar/$hash?s=$size";
+        return "https://dn-qiniu-avatar.qbox.me/avatar/$hash?s=$size";
     }
 }
