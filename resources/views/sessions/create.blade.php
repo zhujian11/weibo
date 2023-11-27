ebc4a4532bb19962c99b0f23ea3f444a1b852877
lebron <595452769@qq.com>
2023-11-27 02:34:39 +0000

    发送找回密码链接

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -16,7 +16,7 @@
             <input type="text" name="email" class="form-control" value="{{ old('email') }}">
           </div>
           <div class="mb-3">
-            <label for="password">密码：</label>
+            <label for="password">密码（<a href="{{ route('password.request') }}">忘记密码</a>）：</label>
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
           </div>
           <div class="mb-3">
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,10 @@ Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 // 激活路由
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+// 忘记密码
+Route::get('password/reset', 'PasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
+
+Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset',  'PasswordController@reset')->name('password.update');
